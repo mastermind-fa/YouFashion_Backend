b4f68e9acf7087345223e21c6ec504cc97e15c57
Farhana Alam <farhanaalam@Farhanas-MacBook-Air.local>
2025-01-27 00:36:18 +0600

    gitignore and database added

--- Clothing_Store/settings.py
+++ Clothing_Store/settings.py
@@ -77,12 +77,17 @@ WSGI_APPLICATION = 'Clothing_Store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Postgres Database
+
+
+
 MEDIA_URL = '/media/'
 
 
